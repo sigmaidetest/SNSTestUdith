6b6dc6cc9f238f77235c71793689c214389fffae
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-11 12:29:51 +0530

    added publish message

--- SNSTestUdith/SNSTestUdith-lambda.js
+++ SNSTestUdith/SNSTestUdith-lambda.js
@@ -1,6 +1,19 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback){
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
+    sns.publish({
+        Message: 'My Newest message',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:480964559519:udith_test'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
 
     console.log(event);
 
